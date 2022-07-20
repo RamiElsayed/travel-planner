e7fb5cc25ffdfff8282d9473ad0a064d1818dbd5
Rami Elsayed <ramielsayed123@outlook.com>
2022-07-20 19:48:08 +0100

    built schema and built database

---
+++ src/config/connection.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+const connection = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        port: 3306,
+      }
+    );
+
+module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const connection = require("./config/connection");
 const routes = require("./routes");
 
 const PORT = process.env.PORT || 4000;
@@ -10,12 +10,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+const init = async () => {
+  try {
+    await connection.sync({ force: false });
+    console.log("'[INFO': DB connection is successful");
 
-const init = () => {
-
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    );
+  } catch (error) {
+    console.log(`[ERROR]: DB connection failed | ${error.message}`);
+  }
 };
 
 init();
